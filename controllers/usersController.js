1f305d742d57c2e39b7502b8a4f3c11c9e39df75
Dennis Anggara <dnnsanggara@gmail.com>
2023-05-19 10:11:45 +0700

    fixed find username

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,30 +12,19 @@ const getAllUsers = (req, res) => {
 }
 
 const getUserByUsername = (req, res) => {
-    if(!req.body.username){
-        User.getAll((err, data) => {
-            if (err) {
+    User.findByUsername(req.body.username, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found user with username: ${req.body.username}.`
+                })
+            } else{
                 res.status(500).send({
-                    message: err.message || "Some error occurred"
+                    message: "Error retrieving user with username: " + req.body.username
                 });
-            } else res.status(200).send(data);
-        })
-    }
-    else {
-        User.findByUsername(req.body.username, (err, data) => {
-            if (err) {
-                if (err.kind === "not_found") {
-                    res.status(404).send({
-                        message: `Not found user with username: ${req.body.username}.`
-                    })
-                } else{
-                    res.status(500).send({
-                        message: "Error retrieving user with username: " + req.body.username
-                    });
-                }
-            } else res.status(200).send(data);
-        })
-    }
+            }
+        } else res.status(200).send(data);
+    })
 }
 
 const createNewUser = (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const userController = require("../controllers/usersController");
 
 router.route('/')
-    .get(userController.getUserByUsername)
+    .get(userController.getAllUsers)
     .post(userController.createNewUser)
     .put(userController.updateUser)
     .delete(userController.deleteUser);
+
+router.route('/login').post(userController.getUserByUsername);
 
 module.exports = router;
