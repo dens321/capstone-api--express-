e27239a750d6f9b6d98a6ebd156fe631879263f1
Dennis Anggara <dnnsanggara@gmail.com>
2023-05-23 23:28:00 +0700

    added jwt authorization

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const PORT = process.env.PORT || 8080;
+const auth = require("./controllers/auth");
 
 
 // using middleware
@@ -14,6 +15,11 @@ app.use(express.urlencoded({ extended: true }))
 // routes
 app.use('/register', require('./routes/register'));
 app.use('/login', require('./routes/login'));
+
+app.get("/welcome", auth, (req, res) => {
+    res.status(200).send(`Welcome! ${req.user.username}`);
+})
+
 
 app.all('*', (req, res) => {
     res.status(404).send('Resource not found')
