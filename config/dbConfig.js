98e7503d84e3d06dc257942d8cc5af03320dd014
Dennis Anggara <dnnsanggara@gmail.com>
2023-05-25 10:31:36 +0700

    added security

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+const config = process.env;
+
 const dbConfig = {
     connectionLimit: 5,
     host: '34.101.85.1',
     user: 'root',
-    password: 'LungCare123',
+    password: config.DB_PASSWORD,
     database: 'capstone'
 }
 
